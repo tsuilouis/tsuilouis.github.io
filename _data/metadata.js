69c2c9c922e2c89be98a67a619e27c4bb03a9b5a
Louis Tsui <louis.tsui@live.ca>
2026-08-01 19:03:31 -0400

    Initialize Eleventy website
    Starter code copied from https://github.com/11ty/eleventy-base-blog.
    Only the files that I think will be relevant for me were copied.

---
+++ _data/metadata.js
@@ -0,0 +1,10 @@
+export default {
+  title: "Louis Tsui",
+  url: "https://louistsui.ca/",
+  language: "en",
+  description: "Personal website with blog and portfolio",
+  author: {
+    name: "Louis Tsui",
+    email: "louis.tsui@live.ca",
+  },
+};
